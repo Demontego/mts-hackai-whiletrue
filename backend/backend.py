8aba104e342c1836da94f79594c99371ce9b4281
Demontego <vkrinat574@yandex.ru>
2021-11-14 09:34:32 +0300

    fix

--- backend/backend.py
+++ backend/backend.py
@@ -14,9 +14,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 all_data = pd.read_csv('../all_data_for_lama.csv', index_col='ID',low_memory=False)
-with open("../automl_rd.pkl", "rb") as f:
+with open("../automl.pkl", "rb") as f:
     model = pickle.load(f)
-    
+
+purchases = np.array(list(model.reader.class_mapping.keys()))
+
 with open("../lbes.pkl", "rb") as f:
     lbes = pickle.load(f)
     
@@ -24,9 +26,10 @@ with open("../ss.pkl", "rb") as f:
     ss = pickle.load(f)
     
 ffm = torch.load('../ffm.pt', map_location='cpu')
+ffm.device = 'cpu'
 
-target = 'MCC_CODE'
-sparse_features = ['ID', 'MCC_CODE','post_index', 'GENDER','REG_CODE', 'MARITAL_STATUS']
+target = ['MCC_CODE']
+sparse_features = ['ID','post_index', 'GENDER','REG_CODE', 'MARITAL_STATUS']
 dense_features = ['PROD_TYPE', 'MM_IN_BANK', 'MM_W_CARD', 'AGE', 
        'EDUCATION_LEVEL', 'DEPENDANT_CNT', 'INCOME_MAIN_AMT']
 
@@ -51,24 +54,26 @@ async def get_id(ID: str = Form(...)):
     if ID not in all_data.index:
         return {'body': test, 'status': 'error'}
     pred_data = pd.DataFrame(all_data.loc[ID]).T
-    pred = model.predict(pred_data).data[:]
+    pred_data['ID'] = pred_data.index
+    pred = model.predict(pred_data).data[0]
     ind = np.argpartition(pred, -10)[-10:]
     prob = pred[ind]
-    classes = pred[ind]
+    classes = purchases[ind]
     ff_data = []
     for ai in classes:
         ff_row={}
         for i in sparse_features:
             if i =='post_index':
-                ff_row['post_index'] = data[str(ai)+'_loc'].iloc[0]
+                if ~np.isnan(pred_data[str(ai)+'_loc'].iloc[0]):
+                    ff_row['post_index'] = str(int(pred_data[str(ai)+'_loc'].iloc[0]))
             else:
-                ff_row[i] = data[i].iloc[0]
+                ff_row[i] = pred_data[i].iloc[0]
         for i in dense_features:
             if i == 'PROD_TYPE':
-                ff_row['PROD_TYPE'] = data['type_card'].iloc[0]
+                ff_row['PROD_TYPE'] = pred_data['type_card'].iloc[0]
             else:
-                ff_row[i] = data[i].iloc[0]
-        ff_row['MCC_CODE'] = ai
+                ff_row[i] = pred_data[i].iloc[0]
+        ff_row['MCC_CODE'] = int(ai)
         ff_data.append(ff_row)
     ff_data = pd.DataFrame(ff_data)
     ff_data[sparse_features+target] = ff_data[sparse_features+target].fillna('-1', )
@@ -83,10 +88,10 @@ async def get_id(ID: str = Form(...)):
     pred_ans = ffm.predict(test_model_input,batch_size=1)
     pred_ans = ss['ts'].inverse_transform(pred_ans)
     test = []
-    for item_id,p,price in zip(classes,prob,pred_ans):
-        row = {'itemid': str(item_id), 'prob': str(p), 'price': str(price)}
+    for item_id,p,price in zip(classes[::-1],prob[::-1],pred_ans[::-1]):
+        row = {'itemid': str(item_id), 'prob': str(p), 'price': str(round(price[0],2))}
         test.append(row)
-    return {'body': test, 'status': 'error'}
+    return {'body': test, 'status': 'ok'}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8010)
